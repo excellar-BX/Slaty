1f0866717d8a5932013d4b2408ee1ada0cb560e1
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2024-12-17 20:37:30 +0100

    Update Waitlist.jsx

--- src/screens/Waitlist.jsx
+++ src/screens/Waitlist.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Waitlist = () => {
   return (
     <div className=" mt-40" >
-      <div className="text-xl my-10 text-center " ></div>
+      <div className="text-xl my-10 text-center " >Join our waitlist</div>
     <div id="getWaitlistContainer" data-waitlist_id="23177" data-widget_type="WIDGET_1"></div>
 </div>
   )
