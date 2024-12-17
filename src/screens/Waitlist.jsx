f95f45dcec2d8ad3a7f8a8871aad8d4c562f448c
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2024-12-17 20:27:32 +0100

    Update Waitlist.jsx

--- src/screens/Waitlist.jsx
+++ src/screens/Waitlist.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 const Waitlist = () => {
   return (
     <div className=" mt-40" >
+      <div className="text-xl text-center " ></div>
     <div id="getWaitlistContainer" data-waitlist_id="23177" data-widget_type="WIDGET_1"></div>
-<link rel="stylesheet" type="text/css" href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"/>
-<script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
-    </div>
+</div>
   )
 }
 
