c73902e53746bd951b8c6add5ac619683654b818
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2024-12-18 08:58:26 +0100

    Update Waitlist.jsx

--- src/screens/Waitlist.jsx
+++ src/screens/Waitlist.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 const Waitlist = () => {
   return (
     <div className=" mt-40" >
       <div className="text-xl text-white my-10 text-center " >Join our waitlist</div>
     <div id="getWaitlistContainer" data-waitlist_id="23177" data-widget_type="WIDGET_1"></div>
-  <link rel="stylesheet" type="text/css" href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"/>
+      <Helmet>
+      <link rel="stylesheet" type="text/css" href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"/>
   <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
-    </div>
+      </Helmet>
+   </div>
   )
 }
 
