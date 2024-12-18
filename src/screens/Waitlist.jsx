c4309136ab178434f78252d265c6ee804be40a1b
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2024-12-18 00:38:44 -0800

    Update Waitlist.jsx

--- src/screens/Waitlist.jsx
+++ src/screens/Waitlist.jsx
@@ -5,10 +5,10 @@ const Waitlist = () => {
   return (
     <div className=" mt-40" >
       <div className="text-xl text-white my-10 text-center " >Join our waitlist</div>
-    <div id="getWaitlistContainer" data-waitlist_id="23177" data-widget_type="WIDGET_1"></div>
+      <div id="getWaitlistContainer" data-waitlist_id="23177" data-widget_type="WIDGET_1"></div>
       <Helmet>
       <link rel="stylesheet" type="text/css" href="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.css"/>
-  <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
+      <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
       </Helmet>
    </div>
   )
