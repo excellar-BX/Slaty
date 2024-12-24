2d2e68af70d7ae8193e055d1cf9cd7cce78da395
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2024-12-24 21:04:37 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
       <Fade duration={2000} cascade damping={0.3} triggerOnce direction='up' >
       <div className=' min-w-[20%] text-center text-orange tracking-widest text-3xl font-bold animate-bounce ' >Slaty</div>
       <div className='text-3xl sm:text-5xl xl:text-7xl font-bold sm:max-w-[60%] text-center leading-relaxed my-5 w-fit mx-auto  ' >Let’s Turn Your Dream Into Reality</div>
-      <div className='text-opacity-30 w-fit mx-auto sm:text-xl text-white sm:max-w-[30%] text-center ' >We bring your vision to life with creativity and precision. Let’s make it happen.</div>
+      <div className='text-opacity-70 w-fit mx-auto sm:text-xl text-white sm:max-w-[30%] text-center ' >We bring your vision to life with creativity and precision. Let’s make it happen.</div>
       <div className=' bg-gradient-to-tr from-orange to-white w-fit text-opacity-0 text-white bg-clip-text text-2xl mx-auto my-10 sm:my-20  ' >Book A Call</div>
       <div>
         <ul className='flex flex-col items-center lg:flex-row justify-center text-xl sm:text-2xl ' >
@@ -34,12 +34,12 @@ const Footer = () => {
               offset={-150}
               key={index}
               to={data.route} >
-              <li className=' sm:mx-5 lg:my-0 my-2 hover:text-opacity-100 ease-in duration-500 cursor-pointer text-white text-opacity-50 ' >{data.Title}</li>
+              <li className=' sm:mx-5 lg:my-0 my-2 hover:text-opacity-100 ease-in duration-500 cursor-pointer text-white text-opacity-70 ' >{data.Title}</li>
             </Link>
           ))}
         </ul>
       </div>
-      <div className='sm:text-2xl  text-center text-white text-opacity-50 my-5 sm:my-10 ' >Copyright <span className='text-orange' >SLATY</span>. All right reserved.</div>
+      <div className='sm:text-2xl  text-center text-white text-opacity-70 my-5 sm:my-10 ' >Copyright <span className='text-orange' >SLATY</span>. All right reserved.</div>
       </Fade>
     </div>
     </div>
