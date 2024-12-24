160192e5dc8eaff0c795ffb9ebe6a27fcd6130d7
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2024-12-24 20:59:44 +0100

    Update Mission.jsx

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -10,7 +10,7 @@ const Mission = () => {
       </AttentionSeeker>
       <Fade cascade triggerOnce direction='up' damping={0.3} duration={2000} >
       <div className='md:text-6xl text-2xl leading-relaxed font-bold max-w-[98%] mx-auto text-center my-10' >Slaty empowers <span className='text-orange mx-1' >students </span>to overcome <span className='text-orange mx-1' >academic challenges</span> with tools that inspire <span className='text-orange mx-1' >focus,motivation,</span>and  <span className='text-orange mx-1' >success</span></div>
-      <div className='text-sm sm:text-xl text-center text-white text-opacity-30 ' >Together, we help every student thrive.</div>
+      <div className='text-sm sm:text-xl text-center text-white text-opacity-70 ' >Together, we help every student thrive.</div>
       </Fade>
       <Reveal triggerOnce >
       <div className=' bg-gradient-to-tr from-orange to-white w-fit text-opacity-0 text-white bg-clip-text text-lg sm:text-2xl mx-auto my-5 sm:my-20  ' >Book A Call</div>
