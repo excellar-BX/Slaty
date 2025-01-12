c64a527f045c8e0f0ba30542160f379f23b476b6
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-01-12 04:31:47 +0100

    Update Waitlist.jsx

--- src/screens/Waitlist.jsx
+++ src/screens/Waitlist.jsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from 'react-router-dom'
 
 const Waitlist = () => {
   return (
-    <div className=" mt-40 mb-20 text-white " >
+    <div className=" mt-40 mb-5 text-white " >
       <Link to='students' >
       <div className='flex justify-center px-5 sm:px-10 py-3 sm:py-5 sm:text-xl mx-auto hover:bg-[#ff9e3d] hover:cursor-pointer items-center mt-10 sm:mt-20  border-spacing-3 border-opacity-10 border border-grey  bg-orange rounded-lg w-fit ' >Join Students WaitList <BiDownArrowAlt className=' bg-white bg-opacity-0 mx-2 text-2xl -rotate-45' /> </div>
       </Link> <Link to='tutors' >
