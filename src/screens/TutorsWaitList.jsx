6e3ec017c017d5deecb7284ff2287327a700ccc6
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-01-18 00:01:58 +0100

    Update TutorsWaitList.jsx

--- src/screens/TutorsWaitList.jsx
+++ src/screens/TutorsWaitList.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Helmet from "react-helmet"
+import { BiDownArrowAlt } from 'react-icons/bi'
+import { Link, } from 'react-router-dom'
 
 const TutorsWaitList = () => {
   return (
@@ -12,7 +14,7 @@ const TutorsWaitList = () => {
     </Helmet>
 
     Link to='/waitlist/students' >
-      <div className='flex justify-center px-5 sm:px-10 py-3 sm:py-5 sm:text-xl mx-auto hover:bg-[#ff9e3d] hover:cursor-pointer items-center mt-10 sm:mt-20  border-spacing-3 border-opacity-10 border border-grey  bg-orange rounded-lg w-fit ' >Join Students WaitList <BiDownArrowAlt className=' bg-white bg-opacity-0 mx-2 text-2xl -rotate-45' /> </div>
+      <div className='flex justify-center text-white px-5 sm:px-10 py-3 sm:py-5 sm:text-xl mx-auto hover:bg-[#ff9e3d] hover:cursor-pointer items-center mt-10 sm:mt-20  border-spacing-3 border-opacity-10 border border-grey  bg-orange rounded-lg w-fit ' >Join Students WaitList <BiDownArrowAlt className=' bg-white bg-opacity-0 mx-2 text-2xl -rotate-45' /> </div>
       </Link>
  </div>
   )
