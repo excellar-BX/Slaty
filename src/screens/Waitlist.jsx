0342331768b4d17c0876abd131f526a576148fdb
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-01-17 22:40:09 +0100

    Update Waitlist.jsx

--- src/screens/Waitlist.jsx
+++ src/screens/Waitlist.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BiDownArrowAlt } from 'react-icons/bi'
-import { Link } from 'react-router-dom'
+import { Link, } from 'react-router-dom'
 
 const Waitlist = () => {
   return (
