0fe80f2b89db7b85b1021431892fc3814683250f
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-01-17 23:51:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,14 +32,14 @@ function App() {
       <Header/>
       <Router>
         <Routes>
-    <Route path='/'>
+         <Route path='/'>
           <Route index element={<Home />} />
           <Route path="waitlist" >
-  <Route index element={<Waitlist/>} >
+          <Route index element={<Waitlist/>} />
           <Route path="tutors" element={<TutorsWaitList/>} />
           <Route path="students" element={<StudentWaitList/>} />
           </Route>
-  </Route>
+         </Route>
         </Routes>
       </Router>
 {/*<Faq/>*/}
