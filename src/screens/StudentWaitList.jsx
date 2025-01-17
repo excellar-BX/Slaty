7a9147c9492c4a040938d28d74a3bfc0915f215c
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-01-18 00:07:38 +0100

    Update StudentWaitList.jsx

--- src/screens/StudentWaitList.jsx
+++ src/screens/StudentWaitList.jsx
@@ -13,7 +13,7 @@ const StudentWaitList = () => {
       <script src="https://prod-waitlist-widget.s3.us-east-2.amazonaws.com/getwaitlist.min.js"></script>
       </Helmet>
 
-      Link to='/waitlist/tutors' >
+      <Link to='/waitlist/tutors' >
       <div className='flex justify-center text-white px-5 sm:px-10 py-3 sm:py-5 sm:text-xl mx-auto hover:bg-[#ff9e3d] hover:cursor-pointer items-center mt-10 sm:mt-20  border-spacing-3 border-opacity-10 border border-grey  bg-orange rounded-lg w-fit ' >Join Tutors WaitList <BiDownArrowAlt className=' bg-white bg-opacity-0 mx-2 text-2xl -rotate-45' /> </div>
       </Link>
    </div>
